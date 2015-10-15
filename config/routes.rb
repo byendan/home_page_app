a073e1310ee2bffe12f2ddb486659305a9c9aab7
byendan <byendanb@gmail.com>
2015-10-14 22:07:41 -0700

    Finish basic layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/about'
+  get 'users/new'
+
+  root              'static_pages#home'
+  get 'home'    =>  'static_pages#home'
+  get 'about'   =>  'static_pages#about'
+  get 'signup'  =>  'users#new'
 
 end
