d1a16fe3c9ecec2e9d4cc8e13b8547c04d4373dd
byendan <byendanb@gmail.com>
2015-10-16 16:39:35 -0700

    New user authorization, and moved some modal load code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update, :show]
+  before_action :correct_user,   only: [:edit, :update, :show]
   
   def show
     @user = User.find(params[:id])
@@ -24,6 +26,20 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                     :password_confirmation)
+    end
+    
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+    
+    # Confirms the ocrrect user.
+    def correct_user
+      @user = User.fin(params[:id])
+      redirect_to(root_url) unless @user == current_user
     end
   
 end
