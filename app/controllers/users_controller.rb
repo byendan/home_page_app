6c04c54b8882f542fa5ec21c849ce7a3c770215a
byendan <byendanb@gmail.com>
2015-10-16 17:09:27 -0700

    Finished the destroyability of users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update, :show]
-  before_action :correct_user,   only: [:edit, :update, :show]
+  before_action :correct_user,   only: [:edit, :update, :show, :destroy]
   
   def show
     @user = User.find(params[:id])
@@ -19,6 +19,12 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to root_path
   end
   
   private
