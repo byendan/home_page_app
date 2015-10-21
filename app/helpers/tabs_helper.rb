1ba7bed5d66fc3a5cddd8da55f754bd2b07b7dde
byendan <byendanb@gmail.com>
2015-10-21 12:30:25 -0700

    Used tab helper to assure links work regardless of http prefix

--- app/helpers/tabs_helper.rb
+++ app/helpers/tabs_helper.rb
@@ -1,2 +1,12 @@
 module TabsHelper
+  
+  # standardizes the beginning of urls
+  def url_fix(tab_url)
+    prefix = "http"
+    if tab_url[0, 4] != prefix
+      tab_url = "http://" << tab_url
+    end
+    return tab_url
+  end
+  
 end
