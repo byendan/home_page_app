fd59c739690435e061bfbee8a303dd4c7f4a7e1a
byendan <byendanb@gmail.com>
2015-10-21 12:09:32 -0700

    Changed signup redirect to root path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
     if @user.save
       log_in @user
       flash[:success] = "Welcome to the Bookmark Page #{@user.name}"
-      redirect_to @user
+      redirect_to root_path
     else
       render 'new'
     end
