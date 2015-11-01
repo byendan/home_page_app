e179d9ec1e8c44d79913c5742a84db0e5f5addf0
byendan <byendanb@gmail.com>
2015-11-01 15:35:22 -0800

    added different link options buttons, put them on the side

--- app/models/sheet.rb
+++ app/models/sheet.rb
@@ -1,6 +1,6 @@
 class Sheet < ActiveRecord::Base
   belongs_to :user
   has_many :tabs, dependent: :destroy
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
   
 end
