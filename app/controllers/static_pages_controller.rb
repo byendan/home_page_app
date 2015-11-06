8368bce0ed1e8ef956152c8f1a92a1078eaaeae2
byendan <byendanb@gmail.com>
2015-11-05 23:21:09 -0800

    use ajax to add tabs, and recieve the new tab information

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -13,6 +13,22 @@ class StaticPagesController < ApplicationController
     set_active_sheet params[:selected_value].to_i
     render :text => "success"
   end
+  
+  def get_sheet
+    render :text=> "success"
+    return current_sheet.id.to_i
+  end
+  
+  def get_new_tab
+    @last_tab = active_sheet.tabs.order(:created_at).last
+    @tab_info = {
+      name: @last_tab.name,
+      address: @last_tab.address,
+      picture: @last_tab.picture
+    }
+    render :text=> "success"
+    return @tab_info.to_json
+  end
 
   
 end
